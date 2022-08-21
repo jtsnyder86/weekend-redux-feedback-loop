c8aa865ee1e20033812eedfff32749af9fc11c11
Josh Snyder <joshua.t.snyder@gmail.com>
2022-08-20 22:18:40 -0500

    worked on the review component and have it mostly set up

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,21 +1,49 @@
 import React from "react";
-import {useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {useDispatch} from 'react-redux'
-import {useSelector} from 'react-redux'
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import axios from "axios";
 
 
 function Review() {
 
-    const review = useSelector ((store) => store.feedbackReducer)
+    cosnt history = useHistory();
+
+    const review = useSelector((store) => store.feedbackReducer);
+
+    const objectToSend = {
+        feeling: review.feeling,
+        understanding: review.understanding,
+        support: review.support,
+        comments: review.comments,
+    }
+
+    const handleSubmit = () => {
+        axios.post('/api/feedback', objectToSend)
+            .then(response => {
+                console.log(response.data);
+                alert('Your submission is complete!')
+                dispatchEvent({
+                    type: ''
+                });
+                history.pushState('/')
+            })
+
+
+
+
+
+    }
 
     return (
         <>
-        <h1>Review</h1>
-        <p>Feeling: {review.feeling}</p>
-        <p>Understanding: {review.understanding}</p>
-        <p>Support: {review.support}</p>
-        <p>Comments: {review.comments}</p>
+            <h1>Review</h1>
+            <p>Feeling: {review.feeling}</p>
+            <p>Understanding: {review.understanding}</p>
+            <p>Support: {review.support}</p>
+            <p>Comments: {review.comments}</p>
+            <button onClick={handleSubmit}>Submit!</button>
         </>
     )
 }
